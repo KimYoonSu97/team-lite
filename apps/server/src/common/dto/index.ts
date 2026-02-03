ff0b21c0bc7826ecb4808fb95694699cb9f41e2d
KimYoonSu97 <zkzk625@naver.com>
2026-02-03 16:58:27 +0900

    feat: 페이지네이션 및 정렬을 위한 공통 DTO 추가 #7
    - PaginationQueryDto: page, limit 파라미터와 skip/take 계산 로직
    - SortQueryDto: sortBy, sortOrder 파라미터 (제네릭 지원)
    - PaginatedResponseDto: 페이지네이션 메타데이터를 포함한 응답 형식
    - 재사용 가능한 공통 DTO로 모든 엔드포인트에서 일관된 페이지네이션 제공

---
+++ apps/server/src/common/dto/index.ts
@@ -0,0 +1,5 @@
+export * from './pagination-query.dto';
+export * from './sort-query.dto';
+export * from './paginated-response.dto';
+export * from './commonResponse.dto';
+export * from './userResponse.dto';
