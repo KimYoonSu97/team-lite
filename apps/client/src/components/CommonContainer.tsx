643f2d08cbaccfe68aef3e9d0b1bacd0a179e1bf
KimYoonSu97 <zkzk625@naver.com>
2026-02-04 22:20:55 +0900

    feat: CommonContainer 및 TeamHeader 컴포넌트 업데이트 #8
    - 공통 레이아웃 컴포넌트 개선
    - 팀 헤더 UI 업데이트

--- apps/client/src/components/CommonContainer.tsx
+++ apps/client/src/components/CommonContainer.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * 공통 컨테이너
+ *
+ * 팀헤더 아래에 위치하는 컨테이너
+ *
+ * 최대넓이 900px
+ */
+
 const CommonContainer = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex justify-center px-8">
